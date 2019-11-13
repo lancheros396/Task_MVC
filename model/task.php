235c6a1bfb6ec8c37641770fdbe581a13e42d755
omar lancheros <lancheros396@gmail.com>
2019-11-13 16:41:02 -0500

    consulta base de datos

--- model/task.php
+++ model/task.php
@@ -1,4 +1,23 @@
 <?php
+class tareas_model{
 
+    private $db;
+    private $tareas;
 
+public function _construct(){
+    require_once("model/connect.php");
+
+    $this->db=conectar::conexion();
+    $this->tareas=array();
+}
+ 
+public function get_tareas(){
+    $consulta=$this->db->query("SELECT * FROM tarea");
+    while ($filas=$consulta->fetch(PDO::FETCH_ASSOC)){
+        $this->tarea[]=$filas;
+    } 
+    
+    return $this->tareas;
+}
+}
 ?>
